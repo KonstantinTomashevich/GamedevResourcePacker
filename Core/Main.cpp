b263fe66bfe349a73ad032b1986e6733468a4153
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-07-31 23:23:54 +0300

    Add boost dependency to build script.

--- Core/Main.cpp
+++ Core/Main.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
+#include <iostream>
+#include <boost/dll.hpp>
 
 int main(int argCount, char **argValues)
 {
+    std::cout << boost::dll::program_location ().string ().c_str () << std::endl;
     return 0;
 }

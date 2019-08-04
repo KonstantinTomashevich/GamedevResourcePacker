f0398d98e609706bfa1ca12b4a369b2fc60a5e54
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-08-04 20:11:19 +0300

    DataObjects plugin: implement DataClass loading from config.

--- Plugins/DataObjects/PluginAPI.cpp
+++ Plugins/DataObjects/PluginAPI.cpp
@@ -1,10 +1,64 @@
 #include "PluginAPI.hpp"
-std::string GamedevResourcePacker::DataObjectsPlugin::PluginAPI::GetName () const
+#include <boost/log/trivial.hpp>
+
+namespace GamedevResourcePacker
+{
+namespace DataObjectsPlugin
+{
+using Path = boost::filesystem::path;
+using DirIterator = boost::filesystem::directory_iterator;
+
+PluginAPI::~PluginAPI ()
+{
+    for (auto &nameDataClassPair : dataClasses_)
+    {
+        delete nameDataClassPair.second;
+    }
+}
+
+std::string PluginAPI::GetName () const
 {
     return "Data Objects";
 }
 
-bool GamedevResourcePacker::DataObjectsPlugin::PluginAPI::Load (const boost::filesystem::path &configFolder)
+bool PluginAPI::Load (const Path &configFolder)
 {
-    return false;
+    Path classesFolder = configFolder / "DataObjectClasses";
+    DirIterator iterator (classesFolder);
+    DirIterator end;
+
+    while (iterator != end)
+    {
+        if (iterator->status ().type () == boost::filesystem::regular_file)
+        {
+            BOOST_LOG_TRIVIAL (info) << "Trying to load data class from " << iterator->path () << ".";
+            try
+            {
+                DataClass *dataClass = new DataClass (iterator->path ());
+                if (dataClasses_.count (dataClass->GetName ()) == 0)
+                {
+                    dataClasses_.insert (std::make_pair (dataClass->GetName (), dataClass));
+                    BOOST_LOG_TRIVIAL (info) << "Loaded class " << dataClass->GetName () << " from file " <<
+                                             iterator->path () << ".";
+                }
+                else
+                {
+                    BOOST_LOG_TRIVIAL (error) << "Unable to load class from " << iterator->path ()
+                                              << " because class with name " <<
+                                              dataClass->GetName () << " already loaded.";
+                }
+            }
+            catch (boost::exception &exception)
+            {
+                BOOST_LOG_TRIVIAL (error) << "Unable to load because of exception: "
+                                          << boost::diagnostic_information (exception);
+            }
+        }
+
+        ++iterator;
+    }
+
+    return true;
+}
+}
 }
--- Plugins/DataObjects/PluginAPI.hpp
+++ Plugins/DataObjects/PluginAPI.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <Core/PluginAPI.hpp>
+#include <unordered_map>
+#include "DataClass.hpp"
 
 namespace GamedevResourcePacker
 {
@@ -8,10 +10,13 @@ namespace DataObjectsPlugin
 class PluginAPI : public GamedevResourcePacker::PluginAPI
 {
 public:
-    virtual ~PluginAPI () = default;
+    virtual ~PluginAPI ();
 
     virtual std::string GetName () const;
     virtual bool Load (const boost::filesystem::path &configFolder);
+
+private:
+    std::unordered_map <std::string, DataClass *> dataClasses_;
 };
 
 extern "C" BOOST_SYMBOL_EXPORT PluginAPI plugin;
